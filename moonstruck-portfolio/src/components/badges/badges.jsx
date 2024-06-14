3fc4c313be61ddaeb88b287edcd5695e05c36b85
MoonstruckDev <dev@moonstruck.pro>
2024-06-14 15:12:45 +0200

    basic implementation of techstack for projects

---
+++ moonstruck-portfolio/src/components/badges/badges.jsx
@@ -0,0 +1,29 @@
+// Import SVG images
+import { ReactComponent as CssBadge } from '../../assets/badges/css_badge.svg';
+import { ReactComponent as HtmlBadge } from '../../assets/badges/html_badge.svg';
+import { ReactComponent as ReactBadge } from '../../assets/badges/react_badge.svg';
+import { ReactComponent as ReduxBadge } from '../../assets/badges/redux_badge.svg';
+import { ReactComponent as JavascriptBadge } from '../../assets/badges/javascript_badge.svg';
+import { ReactComponent as SassBadge } from '../../assets/badges/sass_badge.svg';
+
+// Function to get SVG component based on technology name
+function getBadgeComponent(technology) {
+  switch (technology.toLowerCase()) {
+    case 'css':
+      return <CssBadge />;
+    case 'html':
+      return <HtmlBadge />;
+    case 'react':
+      return <ReactBadge />;
+    case 'redux':
+      return <ReduxBadge />;
+    case 'javascript':
+      return <JavascriptBadge />;
+    case 'scss':
+      return <SassBadge />;
+    default:
+      return null;
+  }
+}
+
+export default getBadgeComponent;
--- moonstruck-portfolio/src/pages/projects/projects.jsx
+++ moonstruck-portfolio/src/pages/projects/projects.jsx
@@ -1,14 +1,28 @@
 // IMPORTS
-import './projects.scss'
-
+import React from 'react';
+import './projects.scss'; // Assuming you have styles defined here
+import projectData from '../../assets/projects.json';
+import getBadgeComponent from '../../components/badges/badges';
 
 // Home page
 function Projects() {
-   return (
-      <section>
-         <h1>Projects</h1>
-      </section>
-   )
+    return (
+       <section>
+          <h1>Projects</h1>
+          {projectData.map((project, index) => (
+             <div key={index}>
+                <h2>{project.title}</h2>
+                <div className="tech-stack">
+                   {project.techstack.map((tech, idx) => (
+                      <div key={idx} className="badge-container">
+                         {getBadgeComponent(tech)}
+                      </div>
+                   ))}
+                </div>
+             </div>
+          ))}
+       </section>
+    );
  }
  
- export default Projects
+ export default Projects;
