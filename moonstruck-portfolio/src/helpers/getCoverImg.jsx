3eedaa06a028955ae3b9f035d8b8b6f9ac0ccf07
MoonstruckDev <dev@moonstruck.pro>
2024-06-15 21:39:20 +0200

    Add new project covers and update getCoverImg helper

--- moonstruck-portfolio/src/helpers/getCoverImg.jsx
+++ moonstruck-portfolio/src/helpers/getCoverImg.jsx
@@ -1,19 +1,19 @@
-import bookiCover from '../assets/images/booki_cover.avif';
-// import ohmyfoodCover from './assets/images/ohmyfood_cover.avif';
-// import printitCover from './assets/images/printit_cover.avif';
-// import sophieBluelCover from './assets/images/sophie_bluel_cover.avif';
-// import kasaCover from './assets/images/kasa_cover.avif';
-// import ninaCarducciCover from './assets/images/nina_carducci_cover.avif';
-// import argentbankCover from './assets/images/argentbank_cover.avif';
+import bookiCover from '../assets/images/covers/booki_cover.avif';
+import ohmyfoodCover from '../assets/images/covers/ohmyfood_cover.avif';
+import printitCover from '../assets/images/covers/printit_cover.avif';
+import sophieBluelCover from '../assets/images/covers/sophie_bluel_cover.avif';
+import kasaCover from '../assets/images/covers/kasa_cover.avif';
+import ninaCarducciCover from '../assets/images/covers/nina_carducci_cover.avif';
+import argentbankCover from '../assets/images/covers/argentbank_cover.avif';
 
 const images = {
   booki_cover: bookiCover,
-//   ohmyfood_cover: ohmyfoodCover,
-//   printit_cover: printitCover,
-//   sophie_bluel_cover: sophieBluelCover,
-//   kasa_cover: kasaCover,
-//   nina_carducci_cover: ninaCarducciCover,
-//   argentbank_cover: argentbankCover,
+  ohmyfood_cover: ohmyfoodCover,
+  printit_cover: printitCover,
+  sophie_bluel_cover: sophieBluelCover,
+  kasa_cover: kasaCover,
+  nina_carducci_cover: ninaCarducciCover,
+  argentbank_cover: argentbankCover,
 };
 
 export default images;
